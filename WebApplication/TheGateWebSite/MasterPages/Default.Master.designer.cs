2dedaa7aa3776f3b18c48b83412f7bfedfab3b1a
koolin <colin.vermander@gmail.com>
2013-04-08 11:02:08 -0400

    Reorg Admin, separate out management section with employee, data export and reports

--- WebApplication/TheGateWebSite/MasterPages/Default.Master.designer.cs
+++ WebApplication/TheGateWebSite/MasterPages/Default.Master.designer.cs
@@ -58,15 +58,6 @@ namespace TheGateWebSite.MasterPages {
         protected global::System.Web.UI.HtmlControls.HtmlAnchor A7;
         
         /// <summary>
-        /// A2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor A2;
-        
-        /// <summary>
         /// HeadLoginView control.
         /// </summary>
         /// <remarks>
